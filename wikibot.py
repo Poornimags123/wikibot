b204782823e67173be173c6af9f6c2e7ed3c2a1c
Poornimags123 <124241098+Poornimags123@users.noreply.github.com>
2023-11-11 12:00:07 +0530

    Add files via upload

---
+++ wikibot.py
@@ -0,0 +1,35 @@
+import wikipedia
+import re
+
+class CelebrityInfoBot:
+    def __init__(self):
+        wikipedia.set_user_agent("CelebrityInfoBot/1.0")
+
+    def get_celebrity_summary(self, name):
+        try:
+            summary = wikipedia.summary(name)
+        except wikipedia.exceptions.DisambiguationError as e:
+            # Handle disambiguation errors if multiple pages are found
+            return f"Multiple options found. Please be more specific: {', '.join(e.options)}"
+
+        except wikipedia.exceptions.HTTPTimeoutError as e:
+            return f"Error: {e}"
+
+        except wikipedia.exceptions.PageError:
+            return "Sorry, information not found."
+
+        return {
+            "name": name,
+            "summary": summary
+        }
+
+# Example usage
+celebrity_bot = CelebrityInfoBot()
+celebrity_name = input("Enter the celebrity name")
+celebrity_info = celebrity_bot.get_celebrity_summary(celebrity_name)
+
+if isinstance(celebrity_info, dict):
+    print(f"Summary for {celebrity_name}:")
+    print(f"{celebrity_info.get('summary', 'Not available')}")
+else:
+    print(celebrity_info)
